9606778f926549d75ebbc28a16f2b1755eb7e112
240174 <240174@E0403-02>
2025-09-24 13:38:56 +0900

    クリックでステージセレクトに飛べるよ

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,5 +1,14 @@
 #include "TitleScene.h"
 #include <DxLib.h>
+#include "Input.h"
+
+namespace
+{
+	int LEFT_TOP_X = 312;
+	int LEFT_TOP_Y = 600;
+	int RIGHT_X = 712;
+	int RIGHT_Y = 700;
+}
 
 TitleScene::TitleScene()
 {
@@ -14,6 +23,19 @@ TitleScene::~TitleScene()
 
 void TitleScene::Update()
 {
+	int mouseX, mouseY;
+	GetMousePoint(&mouseX, &mouseY);
+
+	if (Input::IsButtonDown(MOUSE_INPUT_LEFT))
+	{
+		
+		if (mouseX > LEFT_TOP_X && RIGHT_X > mouseX && mouseY > LEFT_TOP_Y && RIGHT_Y > mouseY)
+		{
+			SceneManager::ChangeScene("StageSelect");
+		}
+		
+
+	}
 	if (CheckHitKey(KEY_INPUT_P)) {
 		SceneManager::ChangeScene("StageSelect");
 	}
@@ -29,4 +51,5 @@ void TitleScene::Draw()
 	DrawExtendGraph(0, 0, 1024, 768, hBGImage_, false);
 	DrawExtendGraph(50, 0, 500, 350, hTitleImage_, true);
 	DrawString(100, 400, "Push [P]Key To Play", GetColor(255, 255, 255));
+	DrawBox(312, 600, 712, 700, GetColor(255, 255, 255), TRUE);
 }
